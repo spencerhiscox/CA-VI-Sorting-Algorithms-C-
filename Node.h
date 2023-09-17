4fbd25e51038f380cc9ddf9a1c5d9014e0e67daf
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2023-09-17 10:04:59 -0400

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,17 @@
+/****************************************/
+/************* Don't Modify *************/
+/****************************************/
+#pragma once
+#include <string>
+
+class Node
+{
+public:
+	Node* next;
+	Node* previous; 
+	std::string gameName;
+	unsigned short int yearReleased;
+	float gamePrice;
+	Node(std::string name = "", unsigned short int year = 0, float price = 0, Node* nextIn = nullptr, Node* prevIn = nullptr);
+};
+
