8697ddb2e447a64ef201939f91bb044be0366e8b
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2023-09-17 10:05:33 -0400

    Create List.h

---
+++ List.h
@@ -0,0 +1,29 @@
+/****************************************/
+/************* Don't Modify *************/
+/****************************************/
+
+#pragma once
+#include "Node.h"
+class List
+{
+	Node* head;
+	Node* tail;
+
+public:
+	List();
+	~List();
+	void Display();
+	int Length();
+	void Delete();
+
+	void PushFront(std::string, unsigned short int, float);
+	Node PopFront();
+	Node PeekFront();
+
+	void PushBack(std::string, unsigned short int, float);
+	Node PopBack();
+	Node PeekBack();
+	void Sort();
+	Node Search(std::string target, signed short int* index);
+};
+
